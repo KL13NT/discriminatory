9523c14c0e6d4fbd8161e9bff282d78fcd1302aa
Nabil <nabil.tharwat@outlook.com>
2020-08-20 11:32:11 +0200

    refactor: made Avatar responsible for fetching passed avatars

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,11 +3,30 @@ import PropTypes from 'prop-types'
 
 import styles from './Avatar.module.sass'
 import cls from '../../utils/cls'
+import { useEffect } from 'react'
+import { getAvatarUrl } from '../../utils/profiles'
+import { useState } from 'react'
 
-function Avatar({ avatar, verified, className }) {
+import Placeholder from '../../assets/placeholder.png'
+
+function Avatar({ avatar, verified, displayName, className }) {
+	const [avatarSrc, setAvatarSrc] = useState(Placeholder)
+
+	useEffect(() => {
+		if (avatar)
+			getAvatarUrl(avatar)
+				.then(avatar => {
+					setAvatarSrc(avatar)
+				})
+				.catch(e => {
+					setAvatarSrc(Placeholder)
+					console.log('COPY THIS WHEN REPORTING', e)
+				})
+	}, [avatar])
+
 	return (
 		<div className={cls(styles.avatar, className)} data-verified={verified}>
-			<img src={avatar} alt={`${name}'s Avatar`} />
+			<img src={avatarSrc} alt={`${displayName}'s Avatar`} />
 		</div>
 	)
 }
