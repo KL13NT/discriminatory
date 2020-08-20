429dacc6d9dac98a851305f2d91bc3cb70c98333
Nabil <nabil.tharwat@outlook.com>
2020-08-20 11:44:17 +0200

    feat: service worker code added

---
+++ src/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open().then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				return (
+					response ||
+					fetch(event.request).then(function(response) {
+						cache.put(event.request, response.clone())
+						return response
+					})
+				)
+			})
+		})
+	)
+})
