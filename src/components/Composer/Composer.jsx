8a0885bacadf5c78a4f3f1f260722192758247bf
Nabil <nabil.tharwat@outlook.com>
2020-08-23 10:33:09 +0200

    feat: Added toggle-able composer from navbar

--- src/components/Composer/Composer.jsx
+++ src/components/Composer/Composer.jsx
@@ -1,19 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import Container from '../Container/Container'
 import Avatar from '../Avatar/Avatar'
 import Textarea from '../Textarea/Textarea'
 import Button from '../Button/Button'
 import LocationPicker from '../LocationPicker/LocationPicker'
+import Overlay from '../Overlay/Overlay'
 
+import { useMutation } from 'urql'
 import { useIntl } from 'react-intl'
+import { useToasts } from '../Toast/Toast'
+import { useComposer } from '../../stores/composer'
+import { useProfile } from '../../stores/profile'
+import { usePosts } from '../../stores/posts'
 
 import styles from './Composer.module.sass'
 
-// REFACTORME: use useCallback & useEffect
+import * as queries from '../../queries/posts'
+
 function Composer({ avatar, verified, onSubmit, ...props }) {
-	const [canSubmit, setCanSubmitState] = useState(false)
 	const { formatMessage: f } = useIntl()
+	const [canSubmit, setCanSubmitState] = useState(false)
 	const [postData, setPostData] = useState({ location: null, content: '' })
 
 	useEffect(() => {
@@ -21,7 +28,7 @@ function Composer({ avatar, verified, onSubmit, ...props }) {
 			setCanSubmitState(true)
 	}, [postData.content, postData.location, postData])
 
-	const onInput = ({ currentTarget }) =>
+	const onChange = ({ currentTarget }) =>
 		setPostData({ ...postData, content: currentTarget.value })
 
 	const onLocationPick = location => setPostData({ ...postData, location })
@@ -48,7 +55,7 @@ function Composer({ avatar, verified, onSubmit, ...props }) {
 						required
 						placeholder={f({ id: 'composer.placeholder' })}
 						value={postData.content}
-						onInput={onInput}
+						onChange={onChange}
 					/>
 					<LocationPicker onPick={onLocationPick} />
 					<Button disabled={!canSubmit} type='submit'>
@@ -60,4 +67,53 @@ function Composer({ avatar, verified, onSubmit, ...props }) {
 	)
 }
 
+function OverlayComposer() {
+	const { add } = useToasts()
+	const { active, toggle } = useComposer()
+	const { profile } = useProfile()
+	const { formatMessage: f } = useIntl()
+	const [postRes, post] = useMutation(queries.post)
+
+	const { home, setHome } = usePosts(state => ({
+		home: state.home,
+		setHome: state.setHome
+	}))
+
+	const error = useCallback(
+		() => add({ text: f({ id: 'errors.general' }), type: 'danger' }),
+		[] // eslint-disable-line
+	)
+
+	useEffect(() => postRes.error && error(), [postRes.error, error])
+
+	const onCompose = newPost => {
+		post(newPost).then(res => {
+			if (!res.error) {
+				add({
+					text: f({ id: 'post.create.success' }),
+					type: 'success'
+				})
+
+				const post = {
+					author: profile,
+					comments: [],
+					reactions: { upvotes: 0, downvotes: 0 },
+					created: Date.now(),
+					...newPost
+				}
+
+				setHome([post, ...home])
+			}
+		})
+	}
+
+	if (!active) return null
+	return (
+		<Overlay onClose={toggle}>
+			<Composer {...profile} verified={profile.verified} onSubmit={onCompose} />
+		</Overlay>
+	)
+}
+
 export default Composer
+export { OverlayComposer }
